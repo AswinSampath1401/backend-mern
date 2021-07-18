91e463d01727977fad4b25bf6dabe9068392527e
Aswin Sampath <aswinsampath1401@gmail.com>
2021-07-18 14:47:14 +0530

    🚑

--- models/sampleSchema.js
+++ models/sampleSchema.js
@@ -5,6 +5,10 @@ const SampleSchema = new Schema({
     task : {
         type:String,
         required: true
+    },
+    user : {
+        type:mongoose.Schema.Types.ObjectId,
+        ref : 'User'
     }
 })
 
--- routes/router.js
+++ routes/router.js
@@ -12,7 +12,15 @@ router.post('/',(req,res)=>{
         task : req.body.task
     })
     console.log(dataTobeInserted);
-    Sample.create(dataTobeInserted).then(res.send('Data Inserted Successfully 😄🙌💥')).catch(err=>res.send(err));
+    // Sample.create(dataTobeInserted).then(res.send('Data Inserted Successfully 😄🙌💥')).catch(err=>res.send(err));
+    Sample.create(dataTobeInserted)
+        .then((dataTobeInserted)=>{
+            dataTobeInserted.user.name = req.user.name
+            dataTobeInserted.save()
+                .then((dataTobeInserted)=>res.json({dataTobeInserted}))
+                .catch(err=>res.json(err))
+        })
+        .catch(err=>res.json(err))
 })
 
 router.put('/:id',(req,res)=>{
